0fd76d224c319541bc81b87874fe4bc858b71eb1
Max <maxnelson997@gmail.com>
2018-02-11 14:46:19 -0700

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts'},
+        { title: 'The Fortune Cookie Principle'},
+        { title: 'Hacking Growth'},
+        { title: 'How to Win Friends and Influence People'}
+    ];
+}
